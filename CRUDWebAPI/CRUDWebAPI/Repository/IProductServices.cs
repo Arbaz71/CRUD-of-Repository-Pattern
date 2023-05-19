630d3c9a2e9b489816ab1ed48916be2858f1dd98
Arbaz Qureshi <arbazqureshi.official@gmail.com>
2023-05-19 17:55:28 +0500

    Create  CRUD-of-Repository-Pattern

---
+++ CRUDWebAPI/CRUDWebAPI/Repository/IProductServices.cs
@@ -0,0 +1,19 @@
+﻿using CRUDWebAPI.Model;
+using WireMock.Admin.Mappings;
+
+namespace CRUDWebAPI.Repository
+{
+    public interface IProductServices
+    {
+        //public ResponseModel Get(int id);
+        //public IEnumerable<ProductServices> GetAll();
+        //public ResponseModel Post(ProductServices products);
+        //public ResponseModel Put(ProductServices products);
+        //public ResponseModel Delete(int id);
+       public Task<IEnumerable<Product>> GetAll();
+        public Task<Product> GetByIdAsync(int id);
+        public Task AddAsync(Product product);
+        public void Update(Product product);
+        public void Delete(Product product);
+    }
+}
---
+++ CRUDWebAPI/CRUDWebAPI/Repository/ProductServices.cs
@@ -0,0 +1,47 @@
+﻿using CRUDWebAPI.Data;
+using CRUDWebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using WireMock.Admin.Mappings;
+
+namespace CRUDWebAPI.Repository
+{
+    public class ProductServices : IProductServices
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        public void Delete(Product product)
+        {
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+        }
+
+        
+    }
+}
+
